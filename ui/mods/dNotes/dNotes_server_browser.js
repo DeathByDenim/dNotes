345bddddd071df0651425eb4873d46f704c39ef1
DeathByDenim <jarno@jarno.ca>
2014-01-16 16:39:35 -0500

    Left test flag on.
    This caused everybody in the lobby to have a note...

--- ui/mods/dNotes/dNotes_server_browser.js
+++ ui/mods/dNotes/dNotes_server_browser.js
@@ -5,7 +5,7 @@ model.processGameBeacon = function(payload, host_id)
 {
 	for(var i = 0; i < payload.player_names.length; i++)
 	{
-		if(!dNotes.hasNote(payload.player_names[i]))
+		if(dNotes.hasNote(payload.player_names[i]))
 		{
 			payload.player_names[i] = '<span><span class="dNotes_playername">' + payload.player_names[i] + '</span><a href="#" onclick="dNotes.createNoteWindow(\'' + (payload.player_names[i]) + '\', {\'rememberPosition\': false, \'offset\': \'center\', \'left\': 0, \'top\': 0, \'containment\': \'.div_body_cont\'}); return false;"><div class="dNotes_icon_note_yes"></div></a></span>';
 		}
