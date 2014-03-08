7cb3514e385cf9bd067be1ebc8ae8d6081cfda2e
DeathByDenim <jarno@jarno.ca>
2014-03-08 15:52:38 -0500

    Social and server browser are working again in gamma build.

--- ui/mods/dNotes/dNotes_server_browser.js
+++ ui/mods/dNotes/dNotes_server_browser.js
@@ -1,17 +1,23 @@
 (function() {
 
 var oldProcessGameBeacon = model.processGameBeacon;
-model.processGameBeacon = function(payload, host_id)
-{
-	for(var i = 0; i < payload.player_names.length; i++)
-	{
-		if(dNotes.hasNote(payload.player_names[i]))
-		{
-			payload.player_names[i] = '<span><span class="dNotes_playername">' + payload.player_names[i] + '</span><a href="#" onclick="dNotes.createNoteWindow(\'' + (payload.player_names[i]) + '\', {\'rememberPosition\': false, \'offset\': \'center\', \'left\': 0, \'top\': 0, \'containment\': \'.div_body_cont\'}); return false;"><div class="dNotes_icon_note_yes"></div></a></span>';
-		}
-	}
-
-	return oldProcessGameBeacon(payload, host_id);
+model.processGameBeacon = function(beacon, host_id) {
+	var game = oldProcessGameBeacon(beacon, host_id);
+	game.player_names = beacon.player_names;
+	
+	return game;
 };
+
+model.testfunc = function(data) {
+	console.log("test!");
+}
+
+var olddatabind = $('.one-game').attr('data-bind');
+$('.one-game').attr('data-bind', olddatabind + ", click: function(data) { $parent.setSelected(data); $parent.testfunc(data); }");
+
+$('.dPlayersInServerBrowser_player-name').replaceWith(
+	'<span><span class="dNotes_icon_note_yes" data-bind="click: function () { dNotes.createNoteWindow($data, {\'rememberPosition\': false, \'offset\': \'center\', \'left\': 0, \'top\': 0}); return false; }, clickBubble: false, visible: dNotes.hasNote($data), attr: {id: dNotes.generateId($data)}"></span><span data-bind="text: $data, css: { dNotes_playername: dNotes.hasNote($data) }" style="margin-right: 1em"></span></span>'
+);
+$('.dPlayersInServerBrowser_player-name').css('white-space', 'nowrap');
 
 })();
--- ui/mods/dNotes/dNotes_social.js
+++ ui/mods/dNotes/dNotes_social.js
@@ -7,59 +7,61 @@ if(localStorage.dNotes_notes)
 	for(var player in notes)
 	{
 		if(dNotes.hasNote(player))
-			model.dNotes_playerswithnotes.push({name: player});
+			model.dNotes_playerswithnotes.push({TrueDisplayName: player});
 	}
 }
 
 $('#social_tabs ul:first-child').append(
 	'            <li class="game_settings">' +
-	'                <a href="#tab_notes">NOTES</a>' +
+	'                <a href="#tab_notes"><loc data-i18n="social:notes.message" desc="">NOTES</loc></a>' +
 	'            </li>'
 );
 $('#social_tabs').append(
 	'        <!--------- NOTES -------------------------->' +
 	'        <div class="div_settings" id="tab_notes">' +
-	'            <span>Players with notes.</span>' +
+	'            <span><loc data-i18n="social:players_with_notes.message" desc="">Players with notes.</loc></span>' +
 	'            <hr/>' +
-	'            <div class="div_settings_graphics_cont">          ' +
-	'                <table>' +
-	'                    <tbody data-bind="foreach: dNotes_playerswithnotes" style="height: 400px">' +
-	'                            <!-- ko if: $index() === model.selectedIndex() -->' +
-	'                            <tr data-bind="click: function () { model.selectedIndex($index()) }" class="tr_selected">' +
-	'                                <td>                         ' +
-	'                                    <div class="td_selected">' +
-	'                                        <span data-bind="text: $data.name"></span>' +
-	'                                    </div>' +                                                                              
-	'                                </td>' +
-	'                            </tr>' +
-	'                            <!-- /ko -->' +
-	'                            <!-- ko ifnot: $index() === model.selectedIndex() -->' +
-	'                            <tr data-bind="click: function () { model.selectedIndex($index()) }" class="tr_not_selected">' +
-	'                                <td>                         ' +
-	'                                    <div class="td_not_selected">' +
-	'                                        <span data-bind="text: $data.name"></span>' +
-	'                                    </div>' +
-	'                                </td>' +
-	'                            </tr>' +
-	'                            <!-- /ko -->' +
+	'            <div class="div_settings_graphics_cont">' +
+	'                <div class="div_settings_graphics_detail_group">' +
+	'                    <table>' +
+	'                        <tbody data-bind="foreach: dNotes_playerswithnotes">' +
+	'' +
+	'                        <!-- ko if: $index() === model.selectedIndex() -->' +
+	'                        <tr data-bind="click: function () { model.selectedIndex($index()) }" class="tr_selected">' +
+	'                            <td>' +
+	'                                <div class="td_selected">' +
+	'                                    <span data-bind="text: $data.TrueDisplayName"></span>' +
+	'                                        <!--<span data-bind="text: $data.id"></span>-->' +
+	'                                        <!--<span data-bind="text: $data.lastGame"></span>-->' +
+	'                                </div>' +
+	'                            </td>' +
+	'                            <td>' +
+	'                            </td>' +
+	'                        </tr>' +
+	'                        <!-- /ko -->' +
+	'                        <!-- ko ifnot: $index() === model.selectedIndex() -->' +
+	'                        <tr data-bind="click: function () { model.selectedIndex($index()) }" class="tr_not_selected">' +
+	'                            <td>' +
+	'                                <div class="td_not_selected">' +
+	'                                    <span data-bind="text: $data.TrueDisplayName"></span>' +
+	'                                        <!--<span data-bind="text: $data.id"></span>-->' +
+	'                                        <!--<span data-bind="text: $data.lastGame"></span>-->' +
+	'                                </div>' +
+	'                            </td>' +
+	'                        </tr>' +
+	'                        <!-- /ko -->' +
+	'' +
 	'                    </tbody>' +
-	'                </table>' +
+	'                    </table>' +
+	'                </div>' +
 	'            </div>' +
 	'        </div>'
 );
-$('.tr_not_selected').prepend(
-	'                                <td style="padding-right: 3px;">                         ' +
-	'                                    <div class="td_selected">' +
-	'                                        <span class="dNotes_icon_note_no" data-bind="click: function () { dNotes.createNoteWindow($data.name, {\'rememberPosition\': false, \'offset\': \'center\', \'left\': 0, \'top\': 0, \'containment\': \'.div_body_cont\'}); }, dNotes_class: dNotes.hasNote($data.name), attr: {id: dNotes.generateId($data.name)}"></span>' +
-	'                                    </div>                                                                              ' +
-	'                                </td>'
-);
-$('.tr_selected').prepend(
-	'                                <td style="padding-right: 3px;">                         ' +
-	'                                    <div class="td_selected">' +
-	'                                        <span class="dNotes_icon_note_no" data-bind="click: function () { dNotes.createNoteWindow($data.name, {\'rememberPosition\': false, \'offset\': \'center\', \'left\': 0, \'top\': 0, \'containment\': \'.div_body_cont\'}); }, dNotes_class: dNotes.hasNote($data.name), attr: {id: dNotes.generateId($data.name)}"></span>' +
-	'                                    </div>                                                                              ' +
-	'                                </td>'
-);
+
+$('.tr_not_selected,.tr_selected').each(function(index) {
+	$(this).children(":first").children(":first").prepend(
+		'                                        <span class="dNotes_icon_note_no" data-bind="click: function () { dNotes.createNoteWindow($data.TrueDisplayName, {\'rememberPosition\': false, \'offset\': \'center\', \'left\': 0, \'top\': 0, \'containment\': \'.div_body_cont\'}); return false; }, clickBubble: false, dNotes_class: dNotes.hasNote($data.TrueDisplayName), attr: {id: dNotes.generateId($data.TrueDisplayName)}"></span>'
+	);
+});
 
 })();
