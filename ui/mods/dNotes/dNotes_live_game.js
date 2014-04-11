a61f60a28714b3d3886bf42fa0982c924f91a551
DeathByDenim <jarno@jarno.ca>
2014-04-10 22:53:24 -0400

    New feature and bugfix
    The new panel that show info about the game now contains Note icons as well.
    During the game, the note icon forced the playername on the next line. This is now fixed.

--- ui/mods/dNotes/dNotes_live_game.js
+++ ui/mods/dNotes/dNotes_live_game.js
@@ -1,1 +1,7 @@
+(function() {
+
 $('.div_primary_color').after('<span class="dNotes_icon_note_no receiveMouse" data-bind="click: function() { dNotes.createNoteWindow($data.name, {\'rememberPosition\': false, \'offset\': \'center\', \'left\': 0, \'top\': 0}); }, dNotes_class: dNotes.hasNote($data.name), attr: {id: dNotes.generateId($data.name)}, clickBubble: false"></span>');
+
+$('.div_name_header').css('width', '189px');
+
+})();
--- ui/mods/dNotes/dNotes_server_browser.js
+++ ui/mods/dNotes/dNotes_server_browser.js
@@ -1,23 +1,10 @@
 (function() {
 
-var oldProcessGameBeacon = model.processGameBeacon;
-model.processGameBeacon = function(beacon, host_id) {
-	var game = oldProcessGameBeacon(beacon, host_id);
-	game.player_names = beacon.player_names;
-	
-	return game;
-};
-
-model.testfunc = function(data) {
-	console.log("test!");
-}
-
-var olddatabind = $('.one-game').attr('data-bind');
-$('.one-game').attr('data-bind', olddatabind + ", click: function(data) { $parent.setSelected(data); $parent.testfunc(data); }");
-
 $('.dPlayersInServerBrowser_player-name').replaceWith(
 	'<span><span class="dNotes_icon_note_yes" data-bind="click: function () { dNotes.createNoteWindow($data, {\'rememberPosition\': false, \'offset\': \'center\', \'left\': 0, \'top\': 0}); return false; }, clickBubble: false, visible: dNotes.hasNote($data), attr: {id: dNotes.generateId($data)}"></span><span data-bind="text: $data, css: { dNotes_playername: dNotes.hasNote($data) }" style="margin-right: 1em"></span></span>'
 );
 $('.dPlayersInServerBrowser_player-name').css('white-space', 'nowrap');
+
+$('.col-players').children('div').prepend('<span class="dNotes_icon_note_yes" data-bind="click: function () { dNotes.createNoteWindow($data, {\'rememberPosition\': false, \'offset\': \'center\', \'left\': 0, \'top\': 0}); return false; }, clickBubble: false, visible: (dNotes.hasNote($data) && $data != \'Open slot\'), attr: {id: dNotes.generateId($data)}"></span>')
 
 })();
